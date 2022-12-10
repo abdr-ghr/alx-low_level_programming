1d8cb0714f88b3ad4f6b0f45da9095693cd3dd62
Abdelrahman Gohar <abd_gohar@hotmail.com>
2022-12-10 14:37:42 -0800

    edited task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
-	char myStr[] = "Programming is like building a multilingual puzzle";
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
 	  puts(myStr);
 	    return 0;
 }
