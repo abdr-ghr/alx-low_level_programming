7d982fefd24b32eb0c9edfd7616c5d91eccf81bb
Abdelrahman Gohar <abd_gohar@hotmail.com>
2022-12-11 05:23:41 -0800

    task 5 and 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(double));
+	printf("%lu\n", sizeof(long));
+	return (0);
+}
