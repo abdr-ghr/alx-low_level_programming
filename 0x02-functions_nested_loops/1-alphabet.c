f5caeb2f1440722dd9a2a8cb8f5e3816e924bf35
Abdelrahman Gohar <abd_gohar@hotmail.com>
2022-12-13 09:28:09 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints alphabet
+ * Return: Always0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	_putchar('ch');
+	}
+	_putchar('\n');
+	return (0);
+}
